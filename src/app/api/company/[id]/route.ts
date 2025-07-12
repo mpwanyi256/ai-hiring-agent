2adfce3193778cc061a57e3b3664ca19ab9d89c7
Samuel Mpwanyi <samuelmpwanyi@gmail.com>
2025-07-12 18:39:39 +0300

    Fix company GET request

--- src/app/api/company/[id]/route.ts
+++ src/app/api/company/[id]/route.ts
@@ -1,24 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
 import { AppRequestParams } from '@/types/api';
-// import { ClientCompany } from '@/types/company';
-// import { SupabaseResponse } from '@/types';
+import { ClientCompany } from '@/types/company';
+import { SupabaseResponse } from '@/types';
 
-// export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-//   try {
-//     const { id } = await params;
-//     const supabase = await createClient();
+export async function GET(request: NextRequest, { params }: AppRequestParams<{ id: string }>) {
+  try {
+    const { id } = await params;
+    const supabase = await createClient();
 
-//     const { data, error } = <SupabaseResponse<ClientCompany>>(
-//       await supabase.from('companies').select('*').eq('id', id).maybeSingle()
-//     );
+    const { data, error } = <SupabaseResponse<ClientCompany>>(
+      await supabase.from('companies').select('*').eq('id', id).maybeSingle()
+    );
 
-//     if (error) return NextResponse.json({ error: error.message }, { status: 500 });
-//     return NextResponse.json({ data });
-//   } catch (error) {
-//     return NextResponse.json({ error }, { status: 500 });
-//   }
-// }
+    if (error) return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ data });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
 
 export async function PUT(request: NextRequest, { params }: AppRequestParams<{ id: string }>) {
   try {
